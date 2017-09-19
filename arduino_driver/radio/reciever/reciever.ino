def58188b9710cae2125ad14cb1c04ea17827e03
Mikael Janhonen <janhon3n@gmail.com>
2017-09-19 22:17:21 +0300

    Create first draft of transmitter

--- arduino_driver/radio/reciever/reciever.ino
+++ arduino_driver/radio/reciever/reciever.ino
@@ -9,12 +9,20 @@ int bluePin = 5;
 RH_ASK driver(2000, dataPin, 13, 14, false);
 
 /*
- * CHANNEL: first 4 bits of the first recieved byte (0-15)
- * COMMAND: 5-8 bits of first byte (0-15)
+ * byte 1, bits 1-4  CHANNEL: The channel the command is ordered to. Execute only if on that channel.
+ * byte 1, bits 5-8  COMMAND: The type of the command
+ * byte 2 RED VALUE
+ * byte 3 GREEN VALUE
+ * byte 4 BLUE VALUE
+ * byte 5 RED VALUE 2
+ * byte 6 GREEN VALUE 2
+ * byte 7 BLUE VALUE 2
+ * byte 8-9 EXTRA DATA (about time)
  * 
  * COMMAND TYPES:
  * 0: Hold - Holds the color given by bytes (2,3,4) (r,g,b)
- * 1: Sweep - Sweeps from the color given by bytes(2,3,4) to the color (5,6,7) over time span given by (8,9)
+ * 1: Sweep - Sweeps from the color given by bytes(2,3,4) to the color (5,6,7) over time span given by (8,9) in unit of 10ms
+ * 2: Blink - Blinks between colors (2,3,4) and (5,6,7) with frequency 1/T where T is (8,9) in unit of 10ms
  */
 
 void setup() {
@@ -26,41 +34,40 @@ void setup() {
   if(!driver.init())
     Serial.println("init failed");
 
+
+  //BLINK TO KNOW IF WORKING
   analogWrite(redPin, 255);
   analogWrite(greenPin, 255);
   analogWrite(bluePin, 255);
-  delay(5000);
+  delay(1000);
   analogWrite(redPin, 0);
   analogWrite(greenPin, 0);
   analogWrite(bluePin, 0);
 }
 
 void loop() {
-  uint8_t buf[8];
+  uint8_t buf[9];
   uint8_t buflen = sizeof(buf);
   if(driver.recv(buf, &buflen)){
     Serial.print("Recieved data length ");
     Serial.println(buflen);
-    if(buflen == 4) { // message size must be 4 bytes (channel, r, g, b)
-      uint8_t channel = buf[0] & 0xf0;
-      uint8_t 
-      uint8_t red = buf[1];
-      uint8_t green = buf[2];
-      uint8_t blue = buf[3];
-      
-      Serial.print(channel);
-      Serial.print(": ");
-      Serial.print(red);
-      Serial.print(",");
-      Serial.print(green);
-      Serial.print(",");
-      Serial.println(blue);
+    uint8_t channel = (buf[0] & 0xf0) >> 4;
+    uint8_t command = buf[0] & 0x0f;
 
+    Serial.print(channel);
+    Serial.print(":");
+    Serial.println(command);
+    uint8_t red = buf[1];
+    uint8_t green = buf[2];
+    uint8_t blue = buf[3];
+    uint8_t red2 = buf[4];
+    uint8_t green2 = buf[5];
+    uint8_t blue2 = buf[6];
+    uint16_t tdata= buf[7]*256 + buf[8];
       
-      analogWrite(redPin, red);
-      analogWrite(greenPin, green);
-      analogWrite(bluePin, blue);
-    }
+    analogWrite(redPin, red);
+    analogWrite(greenPin, green);
+    analogWrite(bluePin, blue);
   }
 }
 
--- arduino_driver/radio/transmitter/transmitter.ino
+++ arduino_driver/radio/transmitter/transmitter.ino
@@ -5,6 +5,11 @@ int dataPin = 10;
 
 RH_ASK driver(2000, 12, dataPin, 14, false);
 
+uint8_t dataBuffer[17];
+int dataBufferLength = 17;
+bool newData = false;
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -13,14 +18,86 @@ void setup() {
 }
 
 void loop() {
-  const uint8_t testData[6] = {1,random(255),random(255),random(255)};
-  driver.send(testData, 4);
+  newData = false;
+  
+  //clear outdated useless data. Only last 9+8 bytes matter
+  while(Serial.available() > 17){
+    Serial.read();
+  }
+  int newDataLength = Serial.available();
+  if(newDataLength > 0){
+    memcpy(dataBuffer, &dataBuffer[newDataLength], dataBufferLength - newDataLength); //shift existing data to the beginning of the array
+    
+    
+    for(int i = 0; i < newDataLength; i++){
+        dataBuffer[dataBufferLength - newDataLength + i] = Serial.read(); // add new data to array
+    }
+    newData = true;
+  }
+  
+  if(newData){
+    Serial.write(dataBuffer, sizeof(dataBuffer));
+    uint8_t endIndex = 255;
+    uint8_t startIndex = 255;
+    for(int i = dataBufferLength - 1; i >= 0; i--){   //go through data array from end towards start
+      if(dataBuffer[i] == 0xff && endIndex == 255){   //if byte is the last end byte
+        endIndex = i;
+      }
+      if(dataBuffer[i] == 0xfe && endIndex != 255){  // if byte is the last start byte before the found end byte
+        startIndex = i;
+      }
+    }
+    Serial.write(startIndex);
+    Serial.write(endIndex);
+
+    if(endIndex != 255 && startIndex != 255 && endIndex > startIndex){
+      //EXECUTE CMD AND SEND DATA
+        uint8_t data[endIndex - startIndex - 1];
+        for(int i = 0; i < endIndex - startIndex - 1; i++){
+          data[i] = dataBuffer[startIndex + 1 + i];
+        }
+        Serial.write(data, sizeof(data));
+        transmitCommand(data);
+
+        
+        //Delete used data
+        for(int i = endIndex; i >= 0; i--){
+          dataBuffer[i] = 0;
+        }
+    }
+  }
+}
+
+
+/*
+ * Transmits the given data over RF
+ */
+void transmitCommand(uint8_t d[]){
   driver.waitPacketSent();
-  Serial.print("Send array: ");
-  Serial.print(testData[1]);
-  Serial.print(",");
-  Serial.print(testData[2]);
-  Serial.print(",");
-  Serial.println(testData[3]);
-  delay(1000);
+  driver.send(d, sizeof(d));
+}
+
+
+
+
+// State variables for executing the command
+int cmd = 0;
+long startTime = 0;
+uint8_t color1[3];
+uint8_t color2[3];
+uint16_t tdata = 0;
+
+/*
+ * inits the given command
+ */
+void executeCommand(uint8_t cmd, uint8_t d[]){
+
+}
+
+
+/*
+ *   updates the led values according to the command and time spent
+ */
+void updateCommand(){
+
 }
