11fbdf97e8dff503afc3b5419abd939dc77b06b1
Mikael Janhonen <janhon3n@gmail.com>
2017-09-22 14:46:30 +0300

    Move the transmitter script to radio_driver folder

---
+++ arduino_driver/radio_driver/transmitter/transmitter.ino
@@ -0,0 +1,99 @@
+#include <RH_ASK.h>
+#include <SPI.h>
+
+int dataPin = 10;
+
+RH_ASK driver(2000, 12, dataPin, 14, false);
+
+uint8_t dataBuffer[17];
+int dataBufferLength = 17;
+bool newData = false;
+
+
+
+void setup() {
+  Serial.begin(9600);
+  if(!driver.init())
+    Serial.println("init failed");
+}
+
+void loop() {
+  newData = false;
+  
+  //clear outdated useless data. Only last 9+8 bytes matter
+  while(Serial.available() > 17){
+    Serial.read();
+  }
+  int newDataLength = Serial.available();
+  if(newDataLength > 0){
+    memcpy(dataBuffer, &dataBuffer[newDataLength], dataBufferLength - newDataLength); //shift existing data to the beginning of the array
+    
+    
+    for(int i = 0; i < newDataLength; i++){
+        dataBuffer[dataBufferLength - newDataLength + i] = Serial.read(); // add new data to array
+    }
+    newData = true;
+  }
+  
+  if(newData){
+    uint8_t endIndex = 255;
+    uint8_t startIndex = 255;
+    for(int i = dataBufferLength - 1; i >= 0; i--){   //go through data array from end towards start
+      if(dataBuffer[i] == 0xff && endIndex == 255){   //if byte is the last end byte
+        endIndex = i;
+      }
+      if(dataBuffer[i] == 0xfe && endIndex != 255){  // if byte is the last start byte before the found end byte
+        startIndex = i;
+      }
+    }
+
+    if(endIndex != 255 && startIndex != 255 && endIndex > startIndex){
+      //EXECUTE CMD AND SEND DATA
+        uint8_t data[endIndex - startIndex - 1];
+        for(int i = 0; i < endIndex - startIndex - 1; i++){
+          data[i] = dataBuffer[startIndex + 1 + i];
+        }
+        transmitCommand(data, endIndex - startIndex - 1);
+        
+        //Delete used data
+        for(int i = endIndex; i >= 0; i--){
+          dataBuffer[i] = 0;
+        }
+    }
+  }
+}
+
+
+/*
+ * Transmits the given data over RF
+ */
+void transmitCommand(uint8_t d[], int siz){
+  Serial.write(d,siz);
+  driver.send(d,siz);
+  driver.waitPacketSent();
+}
+
+
+
+
+// State variables for executing the command
+int cmd = 0;
+long startTime = 0;
+uint8_t color1[3];
+uint8_t color2[3];
+uint16_t tdata = 0;
+
+/*
+ * inits the given command
+ */
+void executeCommand(uint8_t cmd, uint8_t d[]){
+
+}
+
+
+/*
+ *   updates the led values according to the command and time spent
+ */
+void updateCommand(){
+
+}
